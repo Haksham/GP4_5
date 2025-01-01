565c2240e309000f95d8308915a4c0ff8575144d
Haksham <harsh924hashvm@gmail.com>
2025-01-01 09:10:27 +0530

    files by harsh

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function addBook() {
+  // Function to add a new book entry
+  alert('Add Book functionality to be implemented');
+}
+
+function addMember() {
+  // Function to add a new member entry
+  alert('Add Member functionality to be implemented');
+}
+
+function addTransaction() {
+  // Function to add a new transaction entry
+  alert('Add Transaction functionality to be implemented');
+}
